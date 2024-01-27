bf45e4558cd17a61d83dfc0c6171282c127c0877
Camilo Manrique <camilomanriquec@gmail.com>
2024-01-27 12:36:09 -0500

    Remove commented code

--- src/index.ts
+++ src/index.ts
@@ -65,55 +65,7 @@ app.get('/application-valid', async (req, res) => {
 
     res.send({
         isValid
-    })
-
-    /*
-
-    resp = await axios.get(baseUrl + '/application', {
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
-        params: {
-            constraints: JSON.stringify([
-                { "key": "User", "constraint_type": "equals", "value": application.User },
-                { "key": "Status", "constraint_type": "equals", "value": "accepted" }
-            ])
-        }
     });
-
-    const applications = resp.data.response.results;
-
-    console.log('apps ids', JSON.stringify(applications.map((a: any) => a['_id']), null, 2));
-
-    let isValid = true;
-
-    for (const userApplication of applications) {
-        if (userApplication.User === application.User) {
-            console.log('user ap', userApplication);
-
-            let resp = await axios.get(baseUrl + '/job/' + userApplication.Job, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            const job = resp.data.response;
-
-            // Pair of dates for the current job we are evaluating
-            const userStartDate = new Date(job.StartDate);
-            const userEndDate = new Date(job.EndDate);
-
-            const appJob = { start: new Date(applicationJob.StartDate), end: new Date(applicationJob.EndDate) };
-            const userJob = { start: userStartDate, end: userEndDate };
-
-            if (slotIsBusy(appJob, userJob)) {
-                console.log('slot is busy')
-                isValid = false;
-                break;
-            }
-        }
-    }
-
-    */
 });
 
 app.listen(3000, () => {
